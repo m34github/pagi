0738e3c178ee47d75f12ace3f0c732d4c35c2fe7
Makoto Mori <m7-mori@nri.co.jp>
2018-03-03 10:14:39 +0900

    fixes #5 install and setup react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 import Home from './components/Home.jsx';
+import Sandbox from './components/Sandbox.jsx';
 
 ReactDOM.render(
-  <Home />,
-  document.querySelector('#root'),
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sandbox" component={Sandbox} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>,
+  document.querySelector('#root')
 );
